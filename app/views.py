68b22f63d9648dc0e7928b674c45905e6e8a74dc
NMIRI-kmatsubara <matsubara.kazune@nmiri.city.nagoya.jp>
2019-07-01 22:56:40 +0900

    add schedule.html

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+def schedule(request):
+    return render(request, 'app/schedule.html')
