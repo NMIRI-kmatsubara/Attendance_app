51ac0c599a4ea3a059ada12cbaf95680c670fc43
nmiri-KM <nmiri-km@nmiri-KMnoMacBook-puro.local>
2019-07-04 17:30:11 +0900

    edit admin.py

--- app/admin.py
+++ app/admin.py
@@ -14,5 +14,36 @@ class PlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'pl')
     list_display_links = ('id', 'pl')
 
+class MyUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email')
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        (_('Personal info'), {'fields':('is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields':('last_login','date_joined')}),
+    )
+
+    add_fieldsets=(
+        (None, {
+            'classes':('wide',),
+            'fields':('email','password1','password2'),
+        })
+    )
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('')
+
+
+
 admin.site.register(Practice, PracticeAdmin)
 admin.site.register(Place, PlaceAdmin)
