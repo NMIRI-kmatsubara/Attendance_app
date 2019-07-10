f8e7062fb6653b14ba03406ebdeae87d5dd9ac91
nmiri-KM <nmiri-km@nmiri-KMnoMacBook-puro.local>
2019-07-10 17:27:35 +0900

    editting schedule.html

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('schedule/', views.schedule, name='schedule'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
+    path('schedule/<int:practice_id>/', views.schedule_detail, name='schedule_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 from django.views import generic
 from .forms import LoginForm
-from .models import Practice
+from .models import Practice, Attend
 
 # Create your views here.
 def index(request):
@@ -13,6 +13,14 @@ def index(request):
 def schedule(request):
     practices = Practice.objects.all().order_by('date')
     return render(request, 'app/schedule.html', {'practices': practices})
+
+def schedule_detail(request, practice_id):
+    prac = Practice.objects.get(pk=practice_id)
+    d = prac.date
+    Attend_members = Attend.objects.filter(date=d)
+    return render(request, 'app/schedule_detail.html', {'Attend_members': Attend_members})
+
+
 
 class Top(generic.TemplateView):
     template_name = 'app/top.html'
@@ -23,3 +31,4 @@ class Login(LoginView):
 
 class Logout(LoginRequiredMixin, LogoutView):
     template_name = 'app/top.html'
+
